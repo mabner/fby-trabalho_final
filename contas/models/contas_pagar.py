f618b2db19d621760792df7b78ab963f24333247
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-02 23:19:50 -0300

    contas a pagar model

--- contas/models/contas_pagar.py
+++ contas/models/contas_pagar.py
@@ -1,58 +1,35 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from django import utils
 from django.db import models
 from django.db.models.deletion import SET_NULL
+from .forma_pagamento import *
 
 
 class SituacaoPagar(models.Model):
 
-    sit_escolha = [('P', 'Pago'), ('N', 'A pagar')]
+    sit_escolha = [('S', 'Pago'), ('N', 'A pagar')]
 
     escolha = models.CharField(choices=sit_escolha,
                                default='N',
                                max_length=1)
 
-
-# Alterar para tabela com chave
-class FormaPagamento(models.Model):
-
-    forma_escolha = [
-        ('DI', 'Dinheiro'),
-        ('BB', 'Boleto'),
-        ('CC', 'Crédito'),
-        ('CD', 'Débito'),
-        ('DB', 'Depósito'),
-        ('TR', 'Tranferência')
-    ]
-
-    forma_pagamento = models.CharField(
-        choices=forma_escolha,
-        default='DI',
-        max_length=2)
+    def __str__(self):
+        return self.escolha
 
 
-# Alterar para tabela com chave
 class ClassificacaoPagar(models.Model):
 
-    class_escolha = [
-        ('AG', 'Água'),
-        ('LU', 'Luz'),
-        ('TE', 'Telefone'),
-        ('IN', 'Internet'),
-        ('AL', 'Alimentação'),
-        ('HO', 'Hobby'),
-        ('OT', 'Outros'),
-    ]
+    sigla = models.CharField(max_length=2, null=False)
+    descricao = models.CharField(max_length=20, null=False)
 
-    classificacao_pagar = models.CharField(
-        choices=class_escolha,
-        default='OT',
-        max_length=2)
+    def __str__(self):
+        return self.descricao
 
 
 class ContasPagar(models.Model):
+
     data_vencimento = models.DateField(null=False)
-    data_pagamento = models.DateField(default=datetime.now(),
+    data_pagamento = models.DateField(default=utils.timezone.now,
                                       null=False)
     valor = models.FloatField(null=False)
     descricao = models.TextField(max_length=300, null=True)
