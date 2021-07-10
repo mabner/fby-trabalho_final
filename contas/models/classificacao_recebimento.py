a0cc0cc8cfd6b4e025c27ff7b8822d5ba8048ef7
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-10 20:56:37 -0300

    lassif. queryset and find by id

--- contas/models/classificacao_recebimento.py
+++ contas/models/classificacao_recebimento.py
@@ -2,12 +2,27 @@
 from django.db import models
 
 
+class ClassifReceberQuerySet(models.QuerySet):
+
+    # Query para retornar a classificação do recebimento por ID
+    def detalhes_classif_recebimento(self, id):
+        return self.filter(id=id)
+
+
 # Classifica Receber Manager
 class ClassifReceberManager(models.Manager):
+
+    def get_queryset(self):
+        return ClassifReceberQuerySet(self.model, using=self._db)
 
     # Retonar todas as classificações de contas a receber
     def obter_classif_contas_receber(self):
         return self.all()
+
+    # Retornar detalhes da classificação do ID informado
+    def obter_detalhe_classif_recebimento(self, id):
+        return self.get_queryset().detalhes_classif_recebimento(id)
+
 
 # Modelo das classificações das contas a receber
 class ClassificacaoReceber(models.Model):
