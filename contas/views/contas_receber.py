7081a9ee9fc9cfd497add675249e0b39296c3e3b
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-10 18:32:09 -0300

    new contas receber test

--- contas/views/contas_receber.py
+++ contas/views/contas_receber.py
@@ -2,7 +2,7 @@ from contas.models.classificacao_recebimento import ClassificacaoReceber
 from django.http.request import HttpRequest
 from contas.models.forma_pagamento import FormaPagamento
 from contas.models.contas_receber import ContasReceber, SIT_ESCOLHA
-from django.shortcuts import redirect, render, resolve_url
+from django.shortcuts import render
 
 
 def receber(request):
@@ -11,45 +11,45 @@ def receber(request):
 
 
 def detalhar_conta_receber(request, idconta):
-    detalhe_conta_receber = ContasReceber.receber_objects.all(
+    detalhe_conta_receber = ContasReceber.receber_objects.obter_detalhes_conta(
         idconta)
     return render(request, 'receber_detalhes.html', {'detalhe_conta_receber': detalhe_conta_receber})
 
 
 def cadastrar_conta_receber(request: HttpRequest):
 
-    classificacao = ClassificacaoReceber.classif_receber_objects.obter_classif_contas_receber()
-
     if request.method == 'GET':
 
+        classificacao = ClassificacaoReceber.classif_receber_objects.obter_classif_contas_receber()
         situacao = SIT_ESCOLHA
         formapagar = FormaPagamento.pagamentos_objects.obter_formas_pagamento()
 
-        return render('contas_receber.html', {
-            'formapagar': formapagar,
-            'situacao': situacao,
-            'classificacao': classificacao
-        })
+        return render('contas_receber.html', {'formapagar': formapagar,
+                                              'situacao': situacao,
+                                              'classificacao': classificacao
+                                              })
     else:
         dados = request.POST
-        descricao = dados['descricao']
-        data_expectativa = dados['data_expectativa']
-        data_pagamento = dados['data_pagamento']
-        valor = dados['valor']
-        classificacao_id = dados['classificacao']
-        formapagar = dados['formapagar']
-        situacao = dados['situacao']
+        _descricao = dados['descricao']
+        _data_expectativa = dados['data_expectativa']
+        _data_recebimento = dados['data_recebimento']
+        _valor = dados['valor']
+        _classificacao_id = dados['classificacao']
+        _formapagar = dados['formapagar']
+        _situacao = dados['situacao']
 
-        classificacao = ClassificacaoReceber.objects.get(id=classificacao_id)
+        _classificacao = ClassificacaoReceber.objects.get(id=_classificacao_id)
 
         ContasReceber.objects.create(
-            descricao=descricao,
-            data_expectativa=data_expectativa,
-            data_pagamento=data_pagamento,
-            valor=valor,
-            classificacao=classificacao,
-            formapagar=formapagar,
-            situacao=situacao
+            descricao=_descricao,
+            data_expectativa=_data_expectativa,
+            data_recebimento=_data_recebimento,
+            valor=_valor,
+            classificacao=_classificacao,
+            formapagar=_formapagar,
+            situacao=_situacao
         )
 
-        return redirect(resolve_url('index'))
+        contas_receber = ContasReceber.receber_objects.obter_todas_contas_receber()
+
+        return render(request, 'contas_receber.html', {'contas_receber': contas_receber})
