0f8d4450c2519bbf26c619e127f881dc352775d2
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-07-11 04:01:42 -0300

    implements cash flow (foreseen and completed)

--- contas/urls.py
+++ contas/urls.py
@@ -1,42 +1,40 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    # Root
-    path('', views.index, name='index'),
+		# Root
+		path('', views.index, name='index'),
 
-    # Formas de Pagamento
-    path('formaspagamento', views.formas_pagamento, name='formaspagamento'),
-    path('formaspagamento/cadastrar',
-         views.cadastrar_formas_pagamento, name='cadastrar_pagamento'),
-    path('formaspagamento/<int:id>',
-         views.detalhar_forma_pagamento, name='detalhes_forma_pagamento'),
-    path('formaspagamento/apagar/<int:id>',
-         views.apagar_forma_pagamento, name='apagar_forma_pagamento'),
+		# Formas de Pagamento
+		path('formaspagamento', views.formas_pagamento, name='formaspagamento'),
+		path('formaspagamento/cadastrar',
+		     views.cadastrar_formas_pagamento, name='cadastrar_pagamento'),
+		path('formaspagamento/<int:id>',
+		     views.detalhar_forma_pagamento, name='detalhes_forma_pagamento'),
+		path('formaspagamento/apagar/<int:id>',
+		     views.apagar_forma_pagamento, name='apagar_forma_pagamento'),
 
+		# Classificações das contas
+		path('classreceber', views.classificacao_receber, name='classreceber'),
+		path('classpagar', views.classificacao_pagar, name='classpagar'),
 
-    # Classificações das contas
-    path('classreceber', views.classificacao_receber, name='classreceber'),
-    path('classpagar', views.classificacao_pagar, name='classpagar'),
+		# Contas a pagar
+		path('pagar', views.pagar, name='pagar'),
+		path('pagar/<int:idconta>',
+		     views.detalhar_conta_pagar, name='detalhes_pagar'),
+		path('pagar/cadastrar', views.cadastrar_conta_pagar, name='cadastrar_pagar'),
 
+		# Contas a receber
+		path('receber', views.receber, name='receber'),
+		path('receber/<int:idconta>',
+		     views.detalhar_conta_receber, name='detalhes_receber'),
+		path('receber/cadastrar', views.cadastrar_conta_receber,
+		     name='cadastrar_receber'),
 
-    # Contas a pagar
-    path('pagar', views.pagar, name='pagar'),
-    path('pagar/<int:idconta>',
-         views.detalhar_conta_pagar, name='detalhes_pagar'),
-    path('pagar/cadastrar', views.cadastrar_conta_pagar, name='cadastrar_pagar'),
+		# Fluxo de caixa
+		path('fluxo', views.fluxo_caixa, name='fluxo'),
+		path('fluxoprevisto', views.fluxo_previsto, name='fluxo_previsto'),
+		path('fluxorealizado', views.fluxo_realizado, name='fluxo_realizado'),
 
-
-    # Contas a receber
-    path('receber', views.receber, name='receber'),
-    path('receber/<int:idconta>',
-         views.detalhar_conta_receber, name='detalhes_receber'),
-    path('receber/cadastrar', views.cadastrar_conta_receber,
-         name='cadastrar_receber'),
-
-
-    # Fluxo de caixa
-    path('fluxo', views.fluxo_caixa, name='fluxo'),
-
-]
+		]
--- contas/views/fluxo.py
+++ contas/views/fluxo.py
@@ -1,49 +1,75 @@
-from contas.models.contas_receber import ContasReceber
-from contas.models.contas_pagar import ContasPagar
 from django.http.request import HttpRequest
 from django.shortcuts import render
 
+from contas.models.contas_pagar import ContasPagar
+from contas.models.contas_receber import ContasReceber
+
 
 def fluxo_caixa(request: HttpRequest):
-    if request.method == 'GET':
-        return render(request, 'fluxo_gerar.html')
-    else:
-        dados = request.POST
-        inicio_caixa = dados['inicio_caixa']
-        data_inicial = dados['data_inicial'].split('-')
-        data_final = dados['data_final'].split('-')
-        mes_inicio = int(data_inicial[1])
-        mes_final = int(data_final[1])
-        ano = int(data_inicial[0])
-        total_mes = []
+	if request.method == 'GET':
+		return render(request, 'fluxo_gerar.html')
+	else:
+		dados = request.POST
+		inicio_caixa = dados['inicio_caixa']
+		data_inicial = dados['data_inicial'].split('-')
+		data_final = dados['data_final'].split('-')
+		mes_inicio = int(data_inicial[1])
+		mes_final = int(data_final[1])
+		ano = int(data_inicial[0])
+		total_mes = []
 
-        while mes_inicio <= mes_final:
-            mes = {
-                'mes': mes_inicio,
-                'valores_pagar': {
-                    'soma_mes':
-                    ContasPagar.pagar_objects.obter_total_pagar_mes(
-                        mes=mes_inicio, ano=ano),
-                    'soma_classificacao':
-                    ContasPagar.pagar_objects.obter_soma_pagar_classificacoes(
-                        mes=mes_inicio, ano=ano),
-                },
-                'valores_receber': {
-                    'soma_mes':
-                    ContasReceber.receber_objects.obter_total_receber_mes(
-                        mes=mes_inicio, ano=ano),
-                    'soma_classificacao':
-                    ContasReceber.receber_objects.obter_soma_receber_classificacoes(
-                        mes=mes_inicio, ano=ano),
-                },
-                'saldo': float(inicio_caixa) -
-                ContasPagar.pagar_objects.obter_total_pagar_mes(
-                    mes=mes_inicio, ano=ano) +
-                ContasReceber.receber_objects.obter_total_receber_mes(
-                    mes=mes_inicio, ano=ano)
-            }
-            mes_inicio += 1
-            total_mes.append(mes)
+		while mes_inicio <= mes_final:
+			mes = {
+					'mes':             mes_inicio,
+					'valores_pagar':   {
+							'soma_mes':
+								ContasPagar.pagar_objects.obter_total_pagar_mes(
+										mes=mes_inicio, ano=ano),
+							'soma_classificacao':
+								ContasPagar.pagar_objects.obter_soma_pagar_classificacoes(
+										mes=mes_inicio, ano=ano),
+							},
+					'valores_receber': {
+							'soma_mes':
+								ContasReceber.receber_objects.obter_total_receber_mes(
+										mes=mes_inicio, ano=ano),
+							'soma_classificacao':
+								ContasReceber.receber_objects.obter_soma_receber_classificacoes(
+										mes=mes_inicio, ano=ano),
+							},
+					'saldo':           float(inicio_caixa) -
+					                   ContasPagar.pagar_objects.obter_total_pagar_mes(
+							                   mes=mes_inicio, ano=ano) +
+					                   ContasReceber.receber_objects.obter_total_receber_mes(
+							                   mes=mes_inicio, ano=ano)
+					}
+			mes_inicio += 1
+			total_mes.append(mes)
 
-        return render(request, 'fluxo.html',
-                      {'inicio_caixa': inicio_caixa, 'total_mes': total_mes})
+		return render(request, 'fluxo.html',
+		              {'inicio_caixa': inicio_caixa, 'total_mes': total_mes})
+
+
+def fluxo_previsto(request: HttpRequest):
+	_em_aberto = ContasPagar.pagar_objects.obter_total_em_aberto()
+	_a_receber = ContasReceber.receber_objects.obter_total_a_receber()
+	_saldo = _a_receber - _em_aberto
+	total_previsto = {
+			"em_aberto": _em_aberto,
+			"a_receber": _a_receber,
+			"saldo":     _saldo
+			}
+	return render(request, 'fluxo_previsto.html', {'total_previsto': total_previsto})
+
+
+def fluxo_realizado(request: HttpRequest):
+	_pagas = ContasPagar.pagar_objects.obter_total_pago()
+	_recebidas = ContasReceber.receber_objects.obter_total_recebido()
+	_saldo = _recebidas - _pagas
+
+	fluxo_realizado = {
+			"pagas":     _pagas,
+			"recebidas": _recebidas,
+			"saldo":     _saldo
+			}
+	return render(request, 'fluxo_realizado.html', {'fluxo_realizado': fluxo_realizado})
